90b1df14061c091b4e3eb32f9ea2b90971fcdfa9
renan <renan@DESKTOP-JQJION5>
2021-04-02 16:58:46 -0300

    Organização de importes.

--- src/test/java/br/com/impacta/clientes/ClienteServiceTest.java
+++ src/test/java/br/com/impacta/clientes/ClienteServiceTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
